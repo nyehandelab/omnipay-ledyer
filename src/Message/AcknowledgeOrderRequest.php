f703a87d51de843e6c3480796139958ac269ce81
David <david@nyehandel.se>
2022-11-12 12:38:10 +0100

    Add 'acknowledge order'

---
+++ src/Message/AcknowledgeOrderRequest.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+namespace Nyehandel\Omnipay\Ledyer\Message;
+
+use Omnipay\Common\Exception\InvalidRequestException;
+use Omnipay\Common\Exception\InvalidResponseException;
+use Omnipay\Common\Http\Exception\NetworkException;
+use Omnipay\Common\Http\Exception\RequestException;
+
+/**
+ * Creates a Ledyer Checkout order if it does not exist
+ */
+final class AcknowledgeOrderRequest extends AbstractOrderRequest
+{
+    /**
+     * @inheritDoc
+     *
+     * @throws InvalidRequestException
+     */
+    public function getData()
+    {
+        $this->validate(
+            'order_id',
+        );
+
+        return [];
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @throws InvalidResponseException
+     * @throws RequestException when the HTTP client is passed a request that is invalid and cannot be sent.
+     * @throws NetworkException if there is an error with the network or the remote server cannot be reached.
+     */
+    public function sendData($data)
+    {
+        $response = $this->sendRequest(
+            'POST',
+            'orders/' . $this->getOrderId() . '/acknowledge',
+        );
+
+        return new AcknowledgeOrderResponse(
+            $this,
+            $this->getResponseBody($response),
+            $response->getStatusCode()
+        );
+    }
+}
